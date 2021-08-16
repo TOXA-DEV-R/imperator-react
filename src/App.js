5b7a4c6dcdd1cb1f93eae94c3c1d2a64c335c529
Tohir <TOXIR_DEV>
2021-08-16 23:01:49 +0600

    I'm making page About

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import Home from "./containers/home/Home";
+import Home from "./pages/home/Home";
 import { BasicDiv } from "./styles/global";
 import Header from "./containers/header/Header";
 import "./styles/styles.scss";
 import Footer from "./components/footer/Footer";
+import { Switch, Route } from "react-router-dom";
+import PageAbout from "./pages/about/PageAbout";
 
 const App = () => {
   return (
     <BasicDiv>
       <Header />
-      <Home />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={PageAbout} />
+      </Switch>
       <Footer />
     </BasicDiv>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
 import Advantages from "../../components/advantages/Advantages";
-import BestProducts from "../best_products/BestProducts";
+import BestProducts from "../../containers/best_products/BestProducts";
 import Categories from "../../components/categories/Categories";
 import Apps from "../../components/apps/Apps";
-import NewGoods from "../new_goods/NewGoods";
+import NewGoods from "../../containers/new_goods/NewGoods";
 import LookingClothe from "../../components/looking_clothes/LookingClothe";
 import Discount from "../../components/discount/Discount";
 
